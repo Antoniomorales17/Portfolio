96440953fad3fcb1d02e095d3c20edfb30a824c1
Antonio Morales <126864866+Antoniomorales17@users.noreply.github.com>
2023-09-22 08:52:17 +0000

    proyect

--- src/js/component/Works/Works.js
+++ src/js/component/Works/Works.js
@@ -1,39 +1,39 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './Works.css';
 import tikets from '../../../img/wizzz.png';
+import retro2 from '../../../img/retro2.png';
 
 const proyectosData = [
   {
     title: 'Wizz Mail',
     long_description:
-      'Aplicación encargada de gestionar y manipular los tickets de los clientes mediante la inteligencia artificial. Trabajo final realizado en un equipo de 3 personas en un plazo de un mes. Para llevarlo a cabo utilizamos el framework React en la parte del Frontend y Python y Flask en la parte del servidor.',
+      'Aplicación encargada de gestionar y manipular los tickets de los clientes mediante la inteligencia artificial.¡Bienvenidos a futuro!',
     img: tikets,
     link_github: 'https://github.com/Antoniomorales17/WizzMail',
     link_demo: 'https://fronted-ticketing-app.vercel.app/',
+  },
+  {
+    title: 'Quiz VideoGames',
+    long_description:
+      'Aplicación de cuestionario de videojuegos con un estilo retro, desarrollada en React. ¡Demuestra tus conocimientos sobre los videojuegos clásicos y diviértete!.',
+    img: retro2,
+    link_github: 'https://github.com/Antoniomorales17/QuizGames',
+    link_demo: 'https://quizvideogames.vercel.app/',
   },
   // Agrega más proyectos aquí
 ];
 
 const Works = () => {
-  const [showDescription, setShowDescription] = useState(false);
-  const [selectedProject, setSelectedProject] = useState(null);
-
-  const toggleDescription = (index) => {
-    setSelectedProject(index);
-    setShowDescription(!showDescription);
-  };
-
   return (
     <section id="works">
       <h2 className="worksTittle">Mis Proyectos</h2>
       <div className="worksContainer">
         {proyectosData.map((proyecto, index) => (
-          <div className={`project ${showDescription && selectedProject === index ? 'active' : ''}`} key={index}>
+          <div className="project" key={index}>
             <div className="projectCard">
               <h3>{proyecto.title}</h3>
-              <div className="imgContainer" onClick={() => toggleDescription(index)}>
+              <div className="imgContainer">
                 <img src={proyecto.img} alt={proyecto.title} className="projectImg" />
-                
               </div>
               <p className="project-description">{proyecto.long_description}</p>
               
