6ad226bbba1b8cd5f347a9c7192bbfb45e036b1f
Antonio Morales <126864866+Antoniomorales17@users.noreply.github.com>
2023-09-25 11:57:15 +0000

    zaask

--- src/js/component/Footer/Footer.js
+++ src/js/component/Footer/Footer.js
@@ -5,6 +5,11 @@ const Intro = () => {
   return (
     <footer className='footer'>
          &copy; 2023 Antonio Morales
+         
+         <a target="_blank" href="https://www.zaask.es/usuario/antoniomoragimenez" style={{ display: "inline-block", textDecoration: "none" }}>
+  <img src="https://www.zaask.es/widget?user=525472&widget=pro-since" alt="Zaask" style={{ width: "60px", height: "auto", }} />
+</a>
+
     </footer>
   );
 };
